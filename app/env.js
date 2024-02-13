f2c097a4760c27c47112aeb9bf4ba361e9321a75
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-13 11:55:31 -0700

    added environment variables, can now log in

--- app/env.js
+++ app/env.js
@@ -1,6 +1,11 @@
 export const dev = window.location.origin.includes('localhost')
 // NOTE this exported variable will be used in your default axios instance
 export const baseURL = dev ? 'https://sandbox.codeworksacademy.com' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'codeworksclassroom.auth0.com'
+export const audience = 'https://codeworksclassroom.com'
+export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+
+
+// domain: codeworksclassroom.auth0.com
+// audience: https://codeworksclassroom.com
+// clientId: pOXw2OGv1LsYi7LEBmDF04RLkXQvldml
