47d6aee6a22eb885798a24320108c73bc218f660
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-13 10:52:14 -0700

    stubbed out template, added to model

--- app/models/Car.js
+++ app/models/Car.js
@@ -3,6 +3,7 @@ export class Car {
     this.id = data.id || data._id
     this.make = data.make
     this.model = data.model
+    this.imgUrl = data.imgUrl
     this.year = data.year
     this.price = data.price
     this.description = data.description
@@ -12,6 +13,36 @@ export class Car {
     this.creator = data.creator
     this.createdAt = new Date(data.createdAt)
     this.updatedAt = new Date(data.updatedAt)
+  }
+
+  get CardHTMLTemplate() {
+    return `
+    <div class="col-12">
+      <div class="row bg-light rounded shadow border border-dark">
+        <div class="col-md-4 px-0">
+          <img
+            src="https://dealerinspire-image-library-prod.s3.us-east-1.amazonaws.com/images/I3BvjD2zREK0FinQCGoyqYrgkzVq3CNSOH2eNqik.jpg"
+            alt="" class="img-fluid rounded-start car-picture">
+        </div>
+        <div class="col-md-8 p-3">
+          <h2>2004 MAZDA MIATA</h2>
+          <h3>$300</h3>
+          <h3>Listed on 12/12/1200</h3>
+          <div class="d-flex">
+            <h4>Listed by SOME DUDE</h4>
+            <img class="creator-picture"
+              src="https://media0.giphy.com/media/7zApYc8tI0fpsR4Rny/giphy.gif?cid=ecf05e47rjwmcrd6jmfmjmncs5x74gigwtvzhd4hus6b24jt&ep=v1_gifs_related&rid=giphy.gif&ct=g"
+              alt="">
+          </div>
+          <p>ENGINE | COLOR</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eius, sed quisquam eaque impedit voluptatem ex
+            pariatur commodi dolorum sapiente.</p>
+
+        </div>
+      </div>
+    </div>
+    `
+
   }
 }
 
