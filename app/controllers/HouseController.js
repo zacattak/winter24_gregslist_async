9ee53ac389df8077160ac46211072e98f673d620
Zac <step7476@alumni.uidaho.edu>
2024-02-13 16:24:25 -0700

    added id "houseListings" above commented out code in houseView

--- app/controllers/HouseController.js
+++ app/controllers/HouseController.js
@@ -1,10 +1,22 @@
-import { houseService } from "../services/HouseService.js"
-import { Pop } from "../utils/Pop.js"
+import { AppState } from "../AppState.js";
+import { houseService } from "../services/HouseService.js";
+import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
+
+function _drawHouses() {
+    const houses = AppState.houses
+    let htmlString = ''
+    houses.forEach(house => htmlString += house.CardHTMLTemplate)
+    setHTML('houseListings', htmlString)
+
+}
 
 export class HouseController {
     constructor() {
         console.log('House controller loaded');
         this.getHouses()
+
+        AppState.on('houses', _drawHouses)
     }
 
     async getHouses() {
